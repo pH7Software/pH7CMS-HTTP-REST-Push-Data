53c1a27203c087a76f9e96462933c44388c2c71b
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-03-04 23:14:20 +0000

    Update copyright

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
  * @title          Example Usage of the pH7CMS HTTP REST Push Data
  *
  * @author         Pierre-Henry Soria <hello@ph7cms.com>
- * @copyright      (c) 2015, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2015-2016, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  */
 
